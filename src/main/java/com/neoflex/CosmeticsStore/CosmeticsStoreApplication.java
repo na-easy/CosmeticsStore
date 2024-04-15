8a0ccf24b78d4e105b8d6224c746db060a3a8f3e
Anastasia Kolesnikova <koolanastasia@mail.ru>
2024-04-15 20:54:45 +0300

    feature-1-1

---
+++ src/main/java/com/neoflex/CosmeticsStore/CosmeticsStoreApplication.java
@@ -0,0 +1,13 @@
+package com.neoflex.CosmeticsStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CosmeticsStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CosmeticsStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/neoflex/CosmeticsStore/CosmeticsStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neoflex.CosmeticsStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CosmeticsStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
